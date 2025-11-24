26201487ad37e3fa998c0d93d0ecf005a4e40929
fmegali5 <fmegali5@gmail.com>
2025-11-24 20:16:23 +0200

    Delete frontend directory

--- frontend/src/lib/axios.js
+++
@@ -1,7 +0,0 @@
-// lib/axios.js
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5173/api" : "/api",
-  withCredentials: true,
-});
